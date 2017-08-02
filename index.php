502a8d6dfd189b5c9cfa2b1de62460ffb6fe8ae2
lianna200 <lianna200@gmail.com>
2017-08-01 20:13:37 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
-<?php include '../view/header.php'; ?>
+<?php include 'view/header.php'; ?>
 <main>
-    <h1>Shopping Cart - under construction</h1>
+    <h1>Menu</h1>
+    <ul>
+        <li>
+            <a href="product_manager">Product Manager</a>
+        </li>
+        <li>
+            <a href="product_catalog">Product Catalog</a>
+        </li>
+    </ul>
 </main>
-<?php include '../view/footer.php'; ?>
+<?php include 'view/footer.php'; ?>
