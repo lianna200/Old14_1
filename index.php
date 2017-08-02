5d5a903f0beab1f7469b133226fd4f9c92a161d5
lianna200 <lianna200@gmail.com>
2017-08-01 20:08:03 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,37 @@
-<?php include 'view/header.php'; ?>
-<main>
-    <h1>Menu</h1>
-    <ul>
-        <li>
-            <a href="product_manager">Product Manager</a>
-        </li>
-        <li>
-            <a href="product_catalog">Product Catalog</a>
-        </li>
-    </ul>
-</main>
-<?php include 'view/footer.php'; ?>
+<?php
+require('../model/database.php');
+require('../model/category.php');
+require('../model/category_db.php');
+require('../model/product.php');
+require('../model/product_db.php');
+
+$action = filter_input(INPUT_POST, 'action');
+if ($action == NULL) {
+    $action = filter_input(INPUT_GET, 'action');
+    if ($action == NULL) {
+        $action = 'list_products';
+    }
+}  
+
+if ($action == 'list_products') {
+    $category_id = filter_input(INPUT_GET, 'category_id', 
+            FILTER_VALIDATE_INT);
+    if ($category_id == NULL || $category_id == FALSE) {
+        $category_id = 1;
+    }
+
+    $current_category = CategoryDB::getCategory($category_id);
+    $categories = CategoryDB::getCategories();
+    $products = ProductDB::getProductsByCategory($category_id);
+
+    include('product_list.php');
+} else if ($action == 'view_product') {
+    $categories = CategoryDB::getCategories();
+
+    $product_id = filter_input(INPUT_GET, 'product_id', 
+            FILTER_VALIDATE_INT);   
+    $product = ProductDB::getProduct($product_id);
+
+    include('product_view.php');
+}
+?>
